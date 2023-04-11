b64327c0fbcaf3bc28a71667330dce28126f4742
vino <vino0908@outlook.com>
2023-04-11 20:11:30 +0800

    feat: 修改位置字段

--- src/shared/index.tsx
+++ src/shared/index.tsx
@@ -6,22 +6,22 @@ export const fontSizes = [12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24].ma
 export const positions = [
   {
     label: '当前位置',
-    value: -1,
-  },
-  {
-    label: '往上一级位置',
     value: 0,
   },
   {
-    label: '往上二级位置',
+    label: '往上一级位置',
     value: 1,
   },
   {
-    label: '往上三级位置',
+    label: '往上二级位置',
     value: 2,
   },
   {
-    label: '往上四级位置',
+    label: '往上三级位置',
     value: 3,
+  },
+  {
+    label: '往上四级位置',
+    value: 4,
   },
 ]
